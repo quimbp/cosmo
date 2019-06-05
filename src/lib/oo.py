4891061031c1af2b6c4cb549d70264b237b1e63c
Joaquim Ballabrera <joaquim@icm.csic.es>
2019-06-05 13:48:08 +0200

    Clean

--- src/lib/oo.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('toto.geojson') as infile:
-  data = json.load(infile)
-
-print ('File       : ', data['file'])
-print ('Source     : ', data['source'])
-print ('Convenstion: ', data['convention'])
-print ('Variables  : ', data['varnames'])
