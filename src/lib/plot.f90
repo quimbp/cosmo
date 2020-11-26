a86295105ac95927ef3349265bc597af00512384
quimbp <joaquim@icm.csic.es>
2020-11-26 01:36:49 +0100

    Improved plot.f90

--- src/lib/plot.f90
+++ src/lib/plot.f90
@@ -15,18 +15,28 @@ character(len=80)                      :: plotpyname_
 integer                                :: plotpyfid_
 
 contains 
+! ...
+! ====================================================================
+! ...
+subroutine plotx(x,linestyle,title,xlabel,ylabel,showgrid, &
+                 linewidth,xlim,ylim)
 
-subroutine plotx(x,linestyle)
-
-real(dp), dimension(:), intent(in)      :: x
-character(len=*), intent(in), optional :: linestyle
+real(dp), dimension(:), intent(in)           :: x
+character(len=*), intent(in), optional       :: linestyle
+character(len=*), intent(in), optional       :: title
+character(len=*), intent(in), optional       :: xlabel
+character(len=*), intent(in), optional       :: ylabel
+logical, intent(in), optional                :: showgrid
+integer, intent(in), optional                :: linewidth
+real(dp), dimension(2), intent(in), optional :: xlim
+real(dp), dimension(2), intent(in), optional :: ylim
 
 
 ! ... Local variables
 ! ...
 integer iu,i,n
-character(80) filename,word
-character(10) lfmt
+character(80) filename,word,lims
+character(18) lfmt,lwidth
 
 if (present(linestyle)) then
  lfmt = '"'//trim(linestyle)//'"'
@@ -40,7 +50,142 @@ filename = trim(rndname())//'.py'
 iu = unitfree()
 open(iu,file=filename,status='new',form='formatted')
 write(iu,'(A)') 'import matplotlib.pyplot as plt'
-write(iu,'("x = [")',advance="no")
+
+call write_vector(iu,'x',x)
+
+if (present(linewidth)) then
+ write(lwidth,'(",linewidth=",i2)') linewidth
+else
+ lwidth = ''
+endif
+
+write(iu,'(A)') 'plt.plot(x,'//trim(lfmt)//trim(lwidth)//')'
+
+if (present(title)) write(iu,'(A)') 'plt.title("'//trim(title)//'")'
+if (present(xlabel)) write(iu,'(A)') 'plt.xlabel("'//trim(xlabel)//'")'
+if (present(ylabel)) write(iu,'(A)') 'plt.ylabel("'//trim(ylabel)//'")'
+if (present(showgrid)) then
+  if (showgrid) write(iu,'(A)') 'plt.grid(True)'
+endif
+if (present(xlim)) then
+  write(word,*) xlim(1)
+  lims = '('//trim(word)//','
+  write(word,*) xlim(2)
+  lims = trim(lims)//trim(word)//')'
+  write(iu,'(A)') 'plt.xlim('//trim(lims)//')'
+endif
+if (present(ylim)) then
+  write(word,*) ylim(1)
+  lims = '('//trim(word)//','
+  write(word,*) ylim(2)
+  lims = trim(lims)//trim(word)//')'
+  write(iu,'(A)') 'plt.ylim('//trim(lims)//')'
+endif
+
+
+write(iu,'(A)') 'plt.show()'
+close(iu)
+call system('python3 '//trim(filename))
+call system('rm -f '//trim(filename))
+
+
+end subroutine plotx
+! ...
+! ======================================================================
+! ...
+subroutine plotxy(x,y,linestyle,title,xlabel,ylabel,showgrid,  &
+                  linewidth,xlim,ylim)
+
+real(dp), dimension(:), intent(in)           :: x
+real(dp), dimension(:), intent(in)           :: y
+character(len=*), intent(in), optional       :: linestyle
+character(len=*), intent(in), optional       :: title
+character(len=*), intent(in), optional       :: xlabel
+character(len=*), intent(in), optional       :: ylabel
+logical, intent(in), optional                :: showgrid
+integer, intent(in), optional                :: linewidth
+real(dp), dimension(2), intent(in), optional :: xlim
+real(dp), dimension(2), intent(in), optional :: ylim
+
+
+! ... Local variables
+! ...
+integer iu,i,n
+character(80) filename,word,lims
+character(18) lfmt,lwidth
+
+
+if (present(linestyle)) then
+ lfmt = '"'//trim(linestyle)//'"'
+else
+ lfmt = "'-r'"
+endif
+
+n = size(x)
+
+filename = trim(rndname())//'.py'
+iu = unitfree()
+open(iu,file=filename,status='new',form='formatted')
+write(iu,'(A)') 'import matplotlib.pyplot as plt'
+
+call write_vector(iu,'x',x)
+call write_vector(iu,'y',y)
+
+if (present(linewidth)) then
+ write(lwidth,'(",linewidth=",i2)') linewidth
+else
+ lwidth = ''
+endif
+
+write(iu,'(A)') 'plt.plot(x,y,'//trim(lfmt)//trim(lwidth)//')'
+
+
+if (present(title)) write(iu,'(A)') 'plt.title("'//trim(title)//'")'
+if (present(xlabel)) write(iu,'(A)') 'plt.xlabel("'//trim(xlabel)//'")'
+if (present(ylabel)) write(iu,'(A)') 'plt.ylabel("'//trim(ylabel)//'")'
+if (present(showgrid)) then
+  if (showgrid) write(iu,'(A)') 'plt.grid(True)'
+endif
+if (present(xlim)) then
+  write(word,*) xlim(1)
+  lims = '('//trim(word)//','
+  write(word,*) xlim(2)
+  lims = trim(lims)//trim(word)//')'
+  write(iu,'(A)') 'plt.xlim('//trim(lims)//')'
+endif
+if (present(ylim)) then
+  write(word,*) ylim(1)
+  lims = '('//trim(word)//','
+  write(word,*) ylim(2)
+  lims = trim(lims)//trim(word)//')'
+  write(iu,'(A)') 'plt.ylim('//trim(lims)//')'
+endif
+
+
+write(iu,'(A)') 'plt.show()'
+close(iu)
+call system('python3 '//trim(filename))
+call system('rm -f '//trim(filename))
+
+
+end subroutine plotxy
+! ...
+! ======================================================================
+! ...
+subroutine write_vector(iu,label,x)
+
+implicit none
+
+integer, intent(in)                  :: iu
+character(len=*), intent(in)         :: label
+real(dp), dimension(:), intent(in)   :: x
+
+integer i,n
+character(80) word
+
+n = size(x)
+
+write(iu,'(A)',advance='no') trim(label) // ' = ['
 
 do i=1,n
   write(word,*) x(i)
@@ -52,23 +197,8 @@ do i=1,n
     write(iu,'("]")')
   endif
 enddo
-write(iu,'(A)') 'plt.plot(x,'//trim(lfmt)//')'
-write(iu,'(A)') 'plt.show()'
-close(iu)
-call system('python3 '//trim(filename))
-call system('rm -f '//trim(filename))
 
-
-end subroutine plotx
-! ...
-! ======================================================================
-! ...
-subroutine plotxy(x,y)
-
-real(dp), dimension(:), intent(in)      :: x
-real(dp), dimension(:), intent(in)      :: y
-
-end subroutine plotxy
+end subroutine write_vector
 ! ...
 ! ======================================================================
 ! ...
