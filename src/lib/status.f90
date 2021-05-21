63f078f54411a58e62960fee26b4983895c6f2ee
quimbp <joaquim@icm.csic.es>
2021-05-21 10:42:51 +0200

    Add cdf_error in status.f90

--- src/lib/status.f90
+++ src/lib/status.f90
@@ -1,5 +1,7 @@
 module module_status
 
+use netcdf
+
 integer                      :: STATUS_ERROR = 0
 character(len=180)           :: STATUS_TEXT  = ""
 
@@ -16,6 +18,17 @@ write(0,*) 'ERROR: '//trim(STATUS_TEXT)
 stop -1
 
 end subroutine check_status
+! ....
+! ====================================================================
+! ...
+subroutine cdf_error()
+
+if (STATUS_ERROR.ne.NF90_NOERR) then
+  STATUS_TEXT = trim(NF90_STRERROR(STATUS_ERROR))
+  return
+endif
+
+end subroutine cdf_error
 ! ...
 ! ====================================================================
 ! ...
